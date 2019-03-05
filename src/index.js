9019650035c91c593610a8a8f56e147bd696de26
Charlie Hoey <charlie.hoey@gmail.com>
2019-03-05 15:22:28 -0500

    Bug/mouse location on scroll (#5)
    * attempted fix for mouse location inconsistencies when scrolling
    
    * added page scroll compensation to mouse position events

--- src/index.js
+++ src/index.js
@@ -336,8 +336,8 @@ export default class ShadertoyReact extends Component < Props, * > {
     const clientX = e.clientX || e.changedTouches[0].clientX;
     const clientY = e.clientY || e.changedTouches[0].clientY;
 
-    let mouseX = clientX - this.canvasPosition.left;
-    let mouseY = (this.canvasPosition.height - clientY) - this.canvasPosition.top;
+    let mouseX = clientX - this.canvasPosition.left - window.pageXOffset;
+    let mouseY = (this.canvasPosition.height - clientY) - this.canvasPosition.top - window.pageYOffset;
 
     this.mousedown = true;
     this.uniforms.iMouse.value[2] = mouseX;
@@ -348,6 +348,7 @@ export default class ShadertoyReact extends Component < Props, * > {
   }
 
   mouseMove = e => {
+
     const {
       lerp = 1
     } = this.props;
@@ -355,8 +356,8 @@ export default class ShadertoyReact extends Component < Props, * > {
     const clientX = e.clientX || e.changedTouches[0].clientX;
     const clientY = e.clientY || e.changedTouches[0].clientY;
 
-    let mouseX = clientX - this.canvasPosition.left;
-    let mouseY = (this.canvasPosition.height - clientY) - this.canvasPosition.top;
+    let mouseX = clientX - this.canvasPosition.left - window.pageXOffset;
+    let mouseY = (this.canvasPosition.height - clientY) - this.canvasPosition.top - window.pageYOffset;
 
     if (lerp !== 1) {
       this.lastMouseArr[0] = mouseX;
